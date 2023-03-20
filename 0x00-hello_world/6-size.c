5f1a6981dbbede0480987bd6b4c4976a2885417e
Richard <richardwale@live.com>
2023-03-20 12:10:00 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - computer size
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizof(char)(\n));
+	printf("Size of a int: %lu byte(s)", sizeof(int)(\n));
+	printf("Size of a long int: %lu byte(s)", sizof(long int)(\n));
+	printf("Size of a long long int: %lu byte(s)", sizof(long long int)(\n));
+	printf("Size of a float: %lu byte(s)", sizof(float)(\n))
+	return (0);
+}
