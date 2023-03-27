641f9f1fc8460977a12d8072d556680de75cd767
Richard <richardwale@live.com>
2023-03-27 16:08:22 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -11,5 +13,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
