8e3f669d1eab92929e2a00e0e2e10d2d5b5987db
Richard <richardwale@live.com>
2023-04-13 07:52:21 -0700

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,47 +2,73 @@
 #include "main.h"
 
 /**
- * _atoi - change string to an integer
- * @s: string to be converted
+ * _atoi - change string to integer
+ * @s: string
  *
- * Return: 1
+ * Return: int converted from string
  */
-
 int _atoi(char *s)
 {
-	int i, j, n, len, m, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
-	j = 0;
+	d = 0;
 	n = 0;
 	len = 0;
-	m = 0;
+	f = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
 
-	while (i < len && m == 0)
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++j;
+			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (j % 2)
+			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
-			m = 1;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			m = 0;
+			f = 0;
 		}
 		i++;
 	}
 
-	if (m == 0)
+	if (f == 0)
 		return (0);
 
 	return (n);
 }
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: arguments array
+ *
+ * Return: 0, 1
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putcher(char c);
-int _ato(char *s);
+int _putchar(char c);
+int _atoi(char *s);
 
 #endif
