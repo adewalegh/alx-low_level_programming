ceb3d3fde4c16dac0aa0f7f7e97962f4c9ebfdb6
Richard <richardwale@live.com>
2023-04-13 06:43:44 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - arguments
+ * @argc: arguments count
+ * @argv: arguments array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
