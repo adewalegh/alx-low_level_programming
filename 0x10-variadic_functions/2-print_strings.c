3919279ee1ac85056b38cee3712af444891470df
Richard <richardwale@live.com>
2023-04-20 08:26:07 -0700

    3-print_all

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,14 +3,15 @@
 #include <stdarg.h>
 
 /**
- * print_strings - Prints strings, followed by a new line.
- * @separator: The string to be printed between strings.
- * @n: The number of strings passed to the function.
+ * print_strings - print strings
+ * @separator: string between strings
+ * @n: number of strings passed
  * @...: A variable number of strings to be printed.
  *
  * Description: If separator is NULL, it is not printed.
  *              If one of the strings if NULL, (nil) is printed instead.
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
