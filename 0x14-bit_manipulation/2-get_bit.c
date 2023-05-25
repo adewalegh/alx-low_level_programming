f1effb0575dd3322fb6aab2f935fb050a7ad8ad3
Richard <richardwale@live.com>
2023-05-25 09:56:11 -0700

    bit manip

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - bit in a decimal number
+ * @n: number
+ * @index: index of bit
+ *
+ * Return: bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit index to 1
+ * @n: number to change
+ * @index: index
+ *
+ * Return: 1 for success, -1 for failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+
+	return (1);
+}
